98f250493ff3827568ec35544e8abaa1efbb3004
GerasimovSergey2001 <gerasimov.serf@gmail.com>
2026-01-17 16:12:25 +0100

    argparse is added in training pipeline

--- train.py
+++ train.py
@@ -1,9 +1,8 @@
-import numpy as np
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 
+import argparse
 from copy import deepcopy
 
 from src.model.flow_matching import ConditionalFlowMatching
@@ -11,13 +10,13 @@ from src.model.base_models.unet_mlp import TDFlowUnet
 from src.training import TDFlowTrainer
 from src.datasets import PointMassMazeDataset
 
-task = 'reach_top_left'
 
-def main():
+
+def main(task = 'reach_top_left', num_epochs=100):
     dataset = PointMassMazeDataset(task=task)
     train_loader = DataLoader(dataset=dataset, batch_size=1024, shuffle=True)
     gamma = 0.99
-    num_epochs = 500
+    # num_epochs = 500
     ema = 1e-3
     optimizer_config = {
         'lr':1e-4,
@@ -36,7 +35,16 @@ def main():
         device='auto'
     )
     trainer.fit(num_epochs)
-    torch.save(trainer.fm.model.state_dict(), 'td2_cfm_model.pth')
+    torch.save(trainer.fm.model.state_dict(), f'td2_cfm_model_{task}.pth')
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--task', type=str, default='reach_top_left')
+    parser.add_argument('--num_epochs', type=int, default=100)    
+    args = parser.parse_args()
+    
+    print(f"Starting training with args: {args}")
+    main(
+        task=args.task, 
+        num_epochs=args.num_epochs
+        )
